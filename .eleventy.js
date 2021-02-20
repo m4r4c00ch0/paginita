1cd9b47609ef919517b5aaacfd06f12d27748e12
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-02-21 00:45:14 +0100

    refactor: use JSON for finding status updates
    - https://www.11ty.dev/docs/data-template-dir/
    
    it turns out using the JS file is an alternative approach!

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
 module.exports = (config) => {
-  config.addCollection("status-updates", (coll) => {
-    const statusCollection = coll.getFilteredByGlob("src/status-updates/*.md");
-    return statusCollection;
-  });
   return {
     dir: {
       input: "src",
